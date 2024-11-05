8ac5a8412d1bda4d06c297653e4a933015d016b3
CatherineUsme <cate-u12@hotmail.com>
2024-11-04 22:29:50 -0500

    Sidebar con los 2 CRUD:Service y Users, Creacion del servicio para SpaServices y el objeto Response con su ResponseHelper, CRUD SpaServices solo con el Index

---
+++ AppWebSpa/Helpers/ResponseHelper.cs
@@ -0,0 +1,34 @@
+﻿using AppWebSpa.Core;
+
+namespace AppWebSpa.Helpers
+{
+    public static class ResponseHelper<T>
+    {
+        public static Response<T> MakeResponseSuccess(T model, string message = "Tarea realizada con exito")
+        {
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = model,
+            };
+
+        }
+
+        public static Response<T> MakeResponseFail(Exception ex)
+        {
+            return new Response<T>
+            {
+                Errors = new List<string>()
+                {
+                    ex.Message
+                },
+
+                IsSuccess = false,
+                Message = "Error al generar la solicitud",
+
+            };
+
+        }
+    }
+}
---
+++ AppWebSpa/Services/ISpaServicesService.cs
@@ -0,0 +1,39 @@
+﻿using AppWebSpa.Core;
+using AppWebSpa.Data;
+using AppWebSpa.Data.Entities;
+using AppWebSpa.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWebSpa.Services
+{
+    public interface ISpaServicesService
+    {
+        public Task<Response<List<SpaService>>> GetListAsync();
+    }
+
+    public class SpaServicesService : ISpaServicesService
+    {
+        private readonly DataContext _context;
+
+        public SpaServicesService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<List<SpaService>>> GetListAsync()
+        {
+            try
+            {
+                List<SpaService> spaservices = await _context.spaService.ToListAsync();
+
+                return ResponseHelper<List<SpaService>>.MakeResponseSuccess(spaservices);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper<List<SpaService>>.MakeResponseFail(ex);
+            }
+
+
+        }
+    }
+}
